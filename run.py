f91a6b27ad81b4a5cccc65c95f0cccf0bdf52d7d
Urook <uri@rookout.com>
2022-07-05 14:08:48 +0300

    Start Rookout

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app import router
+import rook
 
 
 def main():
+    rook.start(
+        token="XXXXXXXXXXXXXXXX",
+        labels={"env": "dev"},
+        fork=True
+    )
+
     app = FastAPI()
     app.include_router(router)
     app.mount("/", StaticFiles(directory="static", html=True), name="static")
