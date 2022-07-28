14abf686e7bb8a4862224f031c7ff0e0a5fc420a
Ryan Hoffman <hopemanryan@gmail.com>
2022-07-28 10:41:44 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,13 +4,6 @@ from app import router
 
 
 def main():
-    import rook
-    rook.start(
-        token="[Your Rookout Token]",
-        labels={"env": "dev"}
-        # When using multiprocessing, enable fork support to load Rookout in all workers
-        # , fork=True
-    )
     app = FastAPI()
     app.include_router(router)
     app.mount("/", StaticFiles(directory="static", html=True), name="static")
